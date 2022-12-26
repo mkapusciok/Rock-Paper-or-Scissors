72769ec94ce32bd863809347718d55a844e4cd60
mkapusciok <m.kapusciok@interia.pl>
2022-12-26 20:08:09 +0100

    Fixed the statement

--- files/rockPaperScissors.js
+++ files/rockPaperScissors.js
@@ -1,9 +1,9 @@
 console.log("hi");
 const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || 'paper' || 'scissors') {
-        return userInput;
-    } else {
-        console.log('Error! Choose rock, paper or scissors.')
-    }
+  userInput = userInput.toLowerCase();
+  if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+    return userInput;
+  } else {
+    console.log("Error! Choose rock, paper or scissors.");
+  }
 };
